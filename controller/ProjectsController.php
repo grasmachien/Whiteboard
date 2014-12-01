d08f4427225ca783aa3cd1a71532bcae1effc11c
Emielvanbetsbrugge <emiel.van.betsbrugge@student.howest.be>
2014-12-01 18:20:37 +0100

    projectfoto, finetuning eerste project met tekst, video ook al mogelijk

--- controller/ProjectsController.php
+++ controller/ProjectsController.php
@@ -73,54 +73,39 @@ class ProjectsController extends Controller {
 				
 			}
 		}
-	
-
-		// if(!empty($_POST['action'])){
-
-		// 	$insertedproject = $this->projectDAO->insert(array(
-		// 		'nieuwProjectNaam' => $_POST['nieuwProjectNaam']
-		// 	));
-		// 	if(!empty($insertedproject)) {
-
-		// 		$name = $_POST['nieuwProjectNaam'];
-		// 		$this->redirect('index.php?page=board&name='.$name);
-		// 	}
-			
-			
-		// }
-
-		// if (!empty($_SESSION['user'])) {
-		// 	if (!empty($_POST)) {
-		// 		if (empty($_POST['nieuwProjectNaam'])) {
-		// 			$errors['nieuwProjectNaam'] = "Geef een project naam in";
-		// 		}
-		// 		if (empty($_POST['nieuwProjectNaam'])) {
-		// 			$errors['nieuwProjectNaam'] = "Geef een project naam in";
-		// 		}
-			
-		// 	}
-		// }
-
 		
 
 	}
 
+
+
 	public function board() {
 
+		$video = false;
+		$tekst = false;
+
 		$existingTekst = $this->projectDAO->getTekstForProject($_GET['name']);
+		$existingVideo = $this->projectDAO->getVideoForProject($_GET['name']);
 		$this->set('existingTekst', $existingTekst);
+		$this->set('existingVideo', $existingVideo);
 
 		if(!empty($_POST['action'])) {
 			if($_POST['action'] == 'nieuwtekst') {
+				$tekst = true;
 				$this->_nieuwtekst();
+				$this->projectDAO->getTekstForProject($_GET['name']);
+
 			} else if($_POST['action'] == 'postit') {
 				var_dump('postit');
 			} else if($_POST['action'] == 'foto') {
 				var_dump('foto');
-			} else if($_POST['action'] == 'video') {
-				var_dump('video');
+			} else if($_POST['action'] == 'upload') {
+				$this->_uploadVideo();
 			}
 		}
+
+		$this->set('video', $video);
+		$this->set('tekst', $tekst);
 
 	}
 
@@ -130,6 +115,29 @@ class ProjectsController extends Controller {
 				'nieuwtekst' => $_POST['nieuwtekst']
 			));
 
+	}
+
+	public function _uploadVideo(){
+		if(!empty($_POST["name"])
+                    && !empty($_FILES["videofile"])){
+
+                    $type = ($_FILES["videofile"]["type"]);
+               		$path = $_FILES["videofile"]["tmp_name"];
+                    $size = filesize($path);
+
+                    if($type == "video/mp4" && $size <=10000000){
+
+                        $filename = $_POST["name"].".mp4";
+                        $newPath = WWW_ROOT.'uploads'.DS.$filename;
+
+                        move_uploaded_file($path,$newPath);
+
+                        $this->projectDAO->addnew($_POST["name"],$filename);
+
+
+                	}
+
+                }
 	}
 
 
--- view/Projects/board.php
+++ view/Projects/board.php
@@ -7,18 +7,30 @@
 		 	echo $_GET['q'];
 		 } ?>"></li>
 		<li><a class="txt" href="#">tekst</a></li>
+		<li><a class="video" href="#">video</a></li>
 		<li><a href="index.php">Mijn projecten</a></li>
 		<li><a href="">Meldingen</a></li>
 		<li><a class="logout" href="index.php?page=logout">Uitloggen</a></li>
 	</ul>
 </div>
 
-<?php
-	if (!empty($existingTekst)) {
+<?php if (!empty($existingTekst)) {
 		
 	
 	 	foreach($existingTekst as $existing): ?>
 			<p><?php echo $existing["tekst"]; ?></p>
+<?php
+		endforeach; 
+	}
+?>
+
+<?php if (!empty($existingVideo)) {
+		
+	
+	 	foreach($existingVideo as $video): ?>
+			<video autoplay loop width="400" height="220" controls=true>
+				<source src="uploads/<?php echo $video['video']; ?>" type="video/mp4">
+			</video>
 <?php
 		endforeach; 
 	}
@@ -36,9 +48,33 @@
 	
 		<form action="" method="post" enctype="multipart/form-data">
 			<fieldset>
-				<label for="name" id="name">tekst</label><br/>
-				<input type="text" required pattern="[A-Za-z].{4,}" class="name" name="nieuwtekst" placeholder="tekst" value=""/>
-				<input type="submit" name="action" id="btnsubmit" value="nieuwtekst"/>
+
+					<label for="name" id="name">tekst</label><br/>
+					<input type="text" required pattern="[A-Za-z].{4,}" class="name" name="nieuwtekst" placeholder="tekst" value=""/>
+					<input type="submit" name="action" id="btnsubmit" value="nieuwtekst"/>
+
+			</fieldset>
+		</form>
+	</div>
+</section>
+
+<section  id="popvideo" class="hidden" class="uploadblock">
+	<div id="uploadwrapvideo" class="uploadwrap">
+	<header>
+		<h1>Video</h1>
+	</header>
+
+	<div class="close" title="close"></div>
+	
+		<form action="" method="post" enctype="multipart/form-data">
+			<fieldset>
+
+					<label for="name" id="name">name</label><br/>
+					<input type="text" required pattern="[A-Za-z].{4,}" class="name" name="name" placeholder="name" value=""/>
+					<label for="videofile">Video (MP4, max 10MB)</label><br/>
+					<input type="file" name="videofile" required class="addvideo">
+					<input type="submit" name="action" id="btnsubmit" value="upload"/>
+
 			</fieldset>
 		</form>
 	</div>
